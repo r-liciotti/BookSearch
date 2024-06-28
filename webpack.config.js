f840ca0a8c1ababbf4e6d17baca0aaf7c99b648b
r-liciotti <35048026+r-liciotti@users.noreply.github.com>
2024-06-28 20:47:56 +0200

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ const config = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html',
+            favicon: './src/img/favicon.png'
         }),
 
         // Add your plugins here
